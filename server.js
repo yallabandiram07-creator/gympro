825a7f94dd56d02bee4a1b26170d7c2a2b9e7b63
Lakshazna Ram <yallabandiram07@gmail.com>
2026-05-12 12:36:45 +0530

    Fix CORS manually

--- server.js
+++ server.js
@@ -22,7 +22,18 @@ const subscriptionRoutes = require("./routes/subscriptionRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", subscriptionRoutes);
